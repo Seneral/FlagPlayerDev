adc6f4baaa3f6bc92bb3fbb217ee048cfd5270db
Seneral <12800840+Seneral@users.noreply.github.com>
2019-10-22 22:49:55 +0200

    SW combined index cache

--- sw.js
+++ sw.js
@@ -1,32 +1,39 @@
-var CACHE_NAME = 'flagplayer-cache-1';
-var urlsToCache = [
-  '/FlagPlayerDev/style.css',
-  '/FlagPlayerDev/index.html',
-  '/FlagPlayerDev/page.js'
-];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function(event)
+{
+	// Perform install steps
+	event.waitUntil(
+		caches.open('flagplayer-cache-1')
+		.then(function(cache)
+		{
+			console.log('Opened cache');
+			return cache.addAll([
+				'/FlagPlayerDev/style.css',
+				'/FlagPlayerDev/index.html',
+				'/FlagPlayerDev/page.js'
+			]);
+		})
+	);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          console.log('Cached ' + event.request);
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('fetch', function(event)
+{
+	if (event.request.url.match(/\/FlagPlayerDev(|\/|\/index\.html)(\?.*)?$/))
+		event.respondWith(caches.match("/FlagPlayerDev/index.html"));
+	else {
+		event.respondWith(
+			caches.match(event.request)
+			.then(function(response)
+			{
+				// Cache hit - return response
+				if (response)
+				{
+					console.log('Cached ' + event.request.url);
+					return response;
+				}
+				console.log('Loading ' + event.request.url);
+				return fetch(event.request);
+			})
+		);
+	}
 });
